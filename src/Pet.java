9b086975929e028c4b8e3b394562a9af7b93cca0
adkrueger <adkrueger@wpi.edu>
2018-10-25 19:50:18 -0400

    added pet (kinda)

---
+++ src/Pet.java
@@ -0,0 +1,10 @@
+public class Pet extends LivingEntity {
+
+    private String name;
+    private Image image;
+
+    public Pet (String name, Image image)  {
+        super(name, image);
+    }
+        
+}
